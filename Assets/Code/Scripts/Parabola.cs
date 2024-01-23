884f147ff614daba6857632f5b08f5c5c11d656e
beatdones <beatdonesdesigner@gmail.com>
2024-01-23 23:58:17 +0100

    Create PowerBar

--- Assets/Code/Scripts/Parabola.cs
+++ Assets/Code/Scripts/Parabola.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+
 public class Parabola : MonoBehaviour
 {
     public Transform startPoint;
--- Assets/Code/Scripts/ThrowForceBar.cs
+++ Assets/Code/Scripts/ThrowForceBar.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ThrowForceBar : MonoBehaviour
+namespace Algoritcom.TechnicalTest.Ball
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ThrowForceBar : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Image powerBar;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] private float power, maxPower;
+
+        [SerializeField] private float powerCost;
+
+        private bool emptyBar, fullBar;
+
+
+
+        private void Update()
+        {
+            GetCurrentFill();
+        }
+
+        private void GetCurrentFill()
+        {
+            if (power >= maxPower)
+            {
+                emptyBar = false;
+                fullBar = true;
+                power = maxPower;
+            }
+            else if(power <= 0)
+            {
+                emptyBar = true;
+                fullBar = false;
+                power = 0;
+            }
+
+            if (emptyBar)
+            {
+                power += powerCost * Time.deltaTime;
+            }
+            else if (fullBar)
+            {
+                power -= powerCost * Time.deltaTime;
+            }
+
+            powerBar.fillAmount = power / maxPower;
+        }
     }
 }
+
